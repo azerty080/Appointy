f30c599c0e72612296a6711b59635c1b38a0b26d
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-20 18:21:25 +0200

    added login view

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('title', 'Login')
+
+
+@section('content')
+    <h1>Inloggen</h1>
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <div class="">
+            <label for="email">Email</label>
+            <input name="email" type="text">
+        </div>
+
+        <div class="">
+            <label for="password">Wachtwoord</label>
+            <input name="password" type="password">
+        </div>
+
+        <button type="submit">Inloggen</button>
+
+    </form>
+    
+@stop
