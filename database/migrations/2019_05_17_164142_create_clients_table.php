03ab42ff7c7182057451e6ef7466bcfd8ffd880e
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:41:41 +0200

    added create client table migration

---
+++ database/migrations/2019_05_17_164142_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->date('birthdate');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
