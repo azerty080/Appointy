1d98603759d9a346a5e5eb01831d8d0d09f5663c
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:40:04 +0200

    added business model

---
+++ app/Business.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Business extends Model
+{
+    // use SoftDeletes;
+
+    protected $table = 'businesses';
+
+    protected $fillable = [
+        'name', 'description', 'extrainfo'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    
+
+    public function bookmark()
+    {
+        return $this->hasMany('App\Bookmark');
+    }
+
+    public function openinghour()
+    {
+        return $this->hasMany('App\OpeningHour');
+    }
+
+    public function leaveday()
+    {
+        return $this->hasMany('App\LeaveDay');
+    }
+}
