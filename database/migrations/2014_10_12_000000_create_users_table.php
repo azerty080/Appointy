cf30dbfa29726b7770be1e3282d4e00a9e326ece
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:44:44 +0200

    edited create users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            
+            $table->string('township');
+            $table->string('address');
+            $table->string('phonenumber');
+            $table->string('profilepicturepath');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
