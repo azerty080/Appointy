4f06332daabd435d02d19832b9c650366e884928
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:43:26 +0200

    added create openinghours table migration

---
+++ database/migrations/2019_05_17_171653_create_openinghours_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOpeninghoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('openinghours', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            
+            $table->string('day');
+            $table->timestamp('opentime');
+            $table->timestamp('closetime');
+
+            $table->foreign('business_id')->references('id')->on('businesses');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('openinghours');
+    }
+}
