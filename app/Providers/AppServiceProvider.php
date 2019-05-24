59280edb23c643bdfc58082ee2e5600ef20545a8
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 17:10:57 +0200

    added forgotten import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; //NEW: Import Schema
 
 class AppServiceProvider extends ServiceProvider
 {
