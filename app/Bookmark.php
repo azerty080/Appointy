c605eccf95eedd9fa27b7cc9df598f6c63c1a918
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:39:53 +0200

    added bookmark model

---
+++ app/Bookmark.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Bookmark extends Model
+{
+    // use SoftDeletes;
+
+    protected $table = 'bookmarks';
+
+    // protected $fillable = [];
+
+
+    public function business() {
+		return $this->belongsTo('App\Business');
+	}
+
+    public function client() {
+		return $this->belongsTo('App\Client');
+    }
+}
