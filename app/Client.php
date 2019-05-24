89dd74e55511b95ca7bb40d0305014cdca756c36
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:40:17 +0200

    added client model

---
+++ app/Client.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Client extends Model
+{
+    // use SoftDeletes;
+
+    protected $table = 'clients';
+
+    protected $fillable = [
+        'firstname', 'lastname', 'birthdate'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+
+
+    public function bookmark()
+    {
+        return $this->hasMany('App\Bookmark');
+    }
+
+    public function appointment()
+    {
+        return $this->hasMany('App\Appointment');
+    }
+}
