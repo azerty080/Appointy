2f83dbf3bbb7768ad20d85cee7d868333248a3b1
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:42:46 +0200

    added create business table migration

---
+++ database/migrations/2019_05_17_164159_create_businesses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBusinessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('businesses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            
+            $table->string('name');
+            $table->string('description');
+            $table->string('extrainfo');
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('businesses');
+    }
+}
