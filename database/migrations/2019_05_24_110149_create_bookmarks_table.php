eabef0f8b5fc3756cec003ff1d591e6d13bf1c5f
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:42:14 +0200

    added create bookmarks table migration

---
+++ database/migrations/2019_05_24_110149_create_bookmarks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookmarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+                        
+            $table->foreign('business_id')->references('id')->on('businesses');
+            $table->foreign('client_id')->references('id')->on('clients');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+}
