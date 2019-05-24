4e5289696c22895bd99aec67112259c4c83a45ec
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:40:33 +0200

    added leaveday model

---
+++ app/LeaveDay.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\SoftDeletes;
+
+class LeaveDay extends Model
+{
+    // use SoftDeletes;
+
+    protected $table = 'leavedays';
+
+    protected $fillable = [
+        'date', 'repeat'
+    ];
+
+    public function business()
+    {
+        return $this->belongsTo('App\Business');
+    }
+}
