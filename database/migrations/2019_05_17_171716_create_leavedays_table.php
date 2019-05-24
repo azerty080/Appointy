a6c04f3b7259faab22a6bd792a14c73df075fa91
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:43:52 +0200

    added create leavedays table migration

---
+++ database/migrations/2019_05_17_171716_create_leavedays_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeavedaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leavedays', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            
+            $table->date('date');
+            $table->string('repeat');
+
+            $table->foreign('business_id')->references('id')->on('businesses');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leavedays');
+    }
+}
