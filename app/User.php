18a71e1405be1edfbfe169578e5ee7c5074ffa55
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:41:04 +0200

    updated user model

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
@@ -15,8 +16,16 @@ class User extends Authenticatable
      *
      * @var array
      */
+    /*
     protected $fillable = [
         'name', 'email', 'password',
+    ];
+    */
+    // use SoftDeletes;
+    protected $table = 'users';
+
+    protected $fillable = [
+        'email', 'password', 'township', 'address', 'phonenumber', 'profilepicturepath'
     ];
 
     /**
@@ -24,16 +33,30 @@ class User extends Authenticatable
      *
      * @var array
      */
+    /*
     protected $hidden = [
         'password', 'remember_token',
     ];
+    */
+
 
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
+    /*
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    */
+
+
+    public function client() {
+		return $this->hasMany('App\Client');
+	}
+
+    public function business() {
+		return $this->hasOne('App\Business');
+	}
 }
