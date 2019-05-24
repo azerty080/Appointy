ddfb1e82d4d168701ddfc7baf6f6a4477f57fd3d
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:39:40 +0200

    added appointment model

---
+++ app/Appointment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Appointment extends Model
+{
+    // use SoftDeletes;
+
+    protected $table = 'appointments';
+
+    protected $fillable = [
+        'date', 'time', 'details', 'sendreminder'
+    ];
+
+
+    public function business() {
+		return $this->belongsTo('App\Business');
+	}
+
+    public function client() {
+		return $this->belongsTo('App\Client');
+    }
+}
