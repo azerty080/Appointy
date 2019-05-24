427966a48ad0c0e888e55c3247792d41742a8fa6
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-05-24 15:43:03 +0200

    added create appointments table migration

---
+++ database/migrations/2019_05_17_164237_create_appointments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            
+            $table->date('date');
+            $table->timestamp('time');
+            $table->string('details');
+            $table->boolean('sendreminder');
+            
+            $table->foreign('business_id')->references('id')->on('businesses');
+            $table->foreign('client_id')->references('id')->on('clients');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
