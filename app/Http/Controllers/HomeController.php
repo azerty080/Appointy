ce8e7d9cfbeed634e93ce0adf1c165616309d2cb
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-06-09 00:44:34 +0200

    added calendar functionality

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -102,29 +102,68 @@ class HomeController extends Controller
     {
         $id = $request->id;
         $name = $request->name;
+        $addedweeks = $request->addedweeks;
 
-        //$openinghours = Business::with('user')->where('id', $id)->first();
+        $businesshours = OpeningHour::where('business_id', $id)->where('closed', 0)->get();
+
+        $appointmentduration = Business::where('id', $id)->pluck('appointmentduration')[0];
         
         $appointments = Appointment::where('business_id', $id)->get();
 
-        $mondayhours = OpeningHour::where('business_id', $id)->where('dayofweek', 'monday')->get();
-        $tuesdayhours = OpeningHour::where('business_id', $id)->where('dayofweek', 'tuesday')->get();
-        $wednesdayhours = OpeningHour::where('business_id', $id)->where('dayofweek', 'wednesday')->get();
-        $thursdayhours = OpeningHour::where('business_id', $id)->where('dayofweek', 'thursday')->get();
-        $fridayhours = OpeningHour::where('business_id', $id)->where('dayofweek', 'friday')->get();
-        $saturdayhours = OpeningHour::where('business_id', $id)->where('dayofweek', 'saturday')->get();
-        $sundayhours = OpeningHour::where('business_id', $id)->where('dayofweek', 'sunday')->get();
+        $monday = OpeningHour::where('business_id', $id)->where('dayofweek', 'monday')->get();
+        $tuesday = OpeningHour::where('business_id', $id)->where('dayofweek', 'tuesday')->get();
+        $wednesday = OpeningHour::where('business_id', $id)->where('dayofweek', 'wednesday')->get();
+        $thursday = OpeningHour::where('business_id', $id)->where('dayofweek', 'thursday')->get();
+        $friday = OpeningHour::where('business_id', $id)->where('dayofweek', 'friday')->get();
+        $saturday = OpeningHour::where('business_id', $id)->where('dayofweek', 'saturday')->get();
+        $sunday = OpeningHour::where('business_id', $id)->where('dayofweek', 'sunday')->get();
+
+
+        $earliesthour = 1440;
+        $latesthour = 0;
+        
+        $earliesthourstr = '';
+        $latesthourstr = '';
+
+
+        foreach ($businesshours as $businesshour) {
+            $openhour = ((int)explode(':', $businesshour->opentime)[0] * 60) + (int)explode(':', $businesshour->opentime)[1];
+            $closehour = ((int)explode(':', $businesshour->closetime)[0] * 60) + (int)explode(':', $businesshour->closetime)[1];
+
+            if ($openhour < $earliesthour) {
+                $earliesthour = $openhour;
+                $earliesthourstr = $businesshour->opentime;
+            }
+
+            if ($closehour > $latesthour) {
+                $latesthour = $closehour;
+                $latesthourstr = $businesshour->closetime;
+            }
+        }
+
+
+        //print($earliesthourstr);
+        //print($latesthourstr);
 
 
         return view('businesscalendar', compact(
+            'id',
+            'name',
+            'addedweeks',
+            
             'appointments',
-            'mondayhours',
-            'tuesdayhours',
-            'wednesdayhours',
-            'thursdayhours',
-            'fridayhours',
-            'saturdayhours',
-            'sundayhours'
+
+            'appointmentduration',
+            'earliesthour',
+            'latesthour',
+
+            'monday',
+            'tuesday',
+            'wednesday',
+            'thursday',
+            'friday',
+            'saturday',
+            'sunday'
         ));
     }
 }
--- resources/views/businesscalendar.blade.php
+++ resources/views/businesscalendar.blade.php
@@ -4,104 +4,258 @@
 
 
 @section('content')
-    <h1><a href="/"><</a> xx/xx/xxxx - xx/xx/xxxx <a href="/">></a></h1>
+
+    <h1>
+        @if($addedweeks != 0)<a href="{{ route('businesscalendar', ['name' => $name, 'id' => $id, 'addedweek' => $addedweeks - 1]) }}"><</a>@endif
+        {{ Carbon\Carbon::now()->addWeek($addedweeks)->startOfWeek()->format('d/m/Y') }} - {{ Carbon\Carbon::now()->addWeek($addedweeks)->endOfWeek()->format('d/m/Y') }}
+        <a href="{{ route('businesscalendar', ['name' => $name, 'id' => $id, 'addedweek' => $addedweeks + 1]) }}">></a>
+    </h1>
    
     
+    <!--
+    {{ date('d/m/Y') }}
+    <p>{{ date("d/m/Y", strtotime("now")) }}</p>
+    <p>{{ date("d/m/Y", strtotime("10 September 2000")) }}</p>
+    <p>{{ date("d/m/Y", strtotime("+1 day")) }}</p>
+    <p>{{ date("d/m/Y", strtotime("+1 week")) }}</p>
+    <p>{{ date("d/m/Y", strtotime("+1 week 2 days 4 hours 2 seconds")) }}</p>
+    <p>{{ date("d/m/Y", strtotime("next Thursday")) }}</p>
+    <p>{{ date("d/m/Y", strtotime("last Monday")) }}</p>
+    <p>{{ date("d/m/Y", strtotime("Monday")) }}</p>
+-->
+    @php $today = Carbon\Carbon::now() @endphp
+    
+    <p>{{ $date = Carbon\Carbon::now() }}</p>
+    <p>{{ $date->startOfWeek()->format('d-m-Y') }}</p>
+    <p>{{ $date->endOfWeek()->format('d-m-Y') }}</p>
 
-    <h2>Openingsuren</h2>
+    <p>------</p>
 
-        <h3>Maandag</h3>
-        @foreach($mondayhours as $hour)
+    <p>{{ $date->addWeek(1)->format('d-m-Y') }}</p>
 
-            @if($hour->closed)
-                <p>Gesloten</p>
-            @else
-                <p>{{ $hour->opentime }} - {{ $hour->closetime }}</p>
-            @endif
+    <p>{{ $date->endOfWeek()->dayOfWeek }}</p>
+    
+    <p>------</p>
+    <p>------</p>
 
-            @for ($i = (int)$hour->opentime; $i < (int)$hour->closetime; $i++)
-                <p>{{ $i }}u00<p>
+
+    <p>{{ $date->addDays(1)->format('d-m-Y') }}</p>
+
+    @php $startofweek = Carbon\Carbon::now()->addWeek($addedweeks)->startOfWeek() @endphp
+
+    +++
+    <p>{{ $date->addWeek(-1)->format('d-m-Y') }}</p>
+    +++
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                
+
+                <th>Ma {{ $startofweek->format('d/m') }}</th>
+                <th>Di {{ $startofweek->addDays(1)->format('d/m') }}</th>
+                <th>Wo {{ $startofweek->addDays(1)->format('d/m') }}</th>
+                <th>Do {{ $startofweek->addDays(1)->format('d/m') }}</th>
+                <th>Vr {{ $startofweek->addDays(1)->format('d/m') }}</th>
+                <th>Za {{ $startofweek->addDays(1)->format('d/m') }}</th>
+                <th>Zo {{ $startofweek->addDays(1)->format('d/m') }}</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+            @for($i = $earliesthour; $i < $latesthour; $i += $appointmentduration)
+                @php $startofweek = Carbon\Carbon::now()->addWeek($addedweeks)->startOfWeek() @endphp
+
+                <tr>
+                    <td>{{ floor($i/60) }}:@if($i%60 == 0)00 @else{{ $i%60 }} @endif</td>
+
+
+                    <td>
+                        @if($startofweek->isPast())
+                            o
+                        @else
+                            @if(count($monday) == 1)
+                                @if(!$monday[0]->closed)
+                                    @if($monday->opentime_in_min >= $i && $i <= $monday->closetime_in_min)
+                                        <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'monday', 'time' => $i]) }}">Boek een afspraak</a>
+                                    @else
+                                        o
+                                    @endif
+                                @else
+                                    o
+                                @endif
+                            @else
+                                @if(($monday[0]->opentime_in_min <= $i && $i < $monday[0]->closetime_in_min) || ($monday[1]->opentime_in_min <= $i && $i < $monday[1]->closetime_in_min))
+                                    <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'monday', 'time' => $i]) }}">Boek een afspraak</a>
+                                @else
+                                    o
+                                @endif
+                            @endif
+                        @endif
+                    </td>
+
+                    <td>
+                        @if($startofweek->addDays(1)->isPast())
+                            @php $beforetoday = false @endphp
+                            o
+                        @else
+                            @if(count($tuesday) == 1)
+                                @if(!$tuesday[0]->closed)
+                                    @if($tuesday->opentime_in_min >= $i && $i <= $tuesday->closetime_in_min)
+                                        <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'tuesday', 'time' => $i]) }}">Boek een afspraak</a>
+                                    @else
+                                        o
+                                    @endif
+                                @else
+                                    o
+                                @endif
+                            @else
+                                @if(($tuesday[0]->opentime_in_min <= $i && $i < $tuesday[0]->closetime_in_min) || ($tuesday[1]->opentime_in_min <= $i && $i < $tuesday[1]->closetime_in_min))
+                                    <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'tuesday', 'time' => $i]) }}">Boek een afspraak</a>
+                                @else
+                                    o
+                                @endif
+                            @endif
+                        @endif
+                    </td>
+
+                    <td>
+                        @if($startofweek->addDays(1)->isPast())
+                            @php $beforetoday = false @endphp
+                            o
+                        @else
+                            @if(count($wednesday) == 1)
+                                @if(!$wednesday[0]->closed)
+                                    @if($wednesday->opentime_in_min >= $i && $i <= $wednesday->closetime_in_min)
+                                        <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'wednesday', 'time' => $i]) }}">Boek een afspraak</a>
+                                    @else
+                                        o
+                                    @endif
+                                @else
+                                    o
+                                @endif
+                            @else
+                                @if(($wednesday[0]->opentime_in_min <= $i && $i < $wednesday[0]->closetime_in_min) || ($wednesday[1]->opentime_in_min <= $i && $i < $wednesday[1]->closetime_in_min))
+                                    <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'wednesday', 'time' => $i]) }}">Boek een afspraak</a>
+                                @else
+                                    o
+                                @endif
+                            @endif
+                        @endif
+                    </td>
+
+                    <td>
+                        @if($startofweek->addDays(1)->isPast())
+                            @php $beforetoday = false @endphp
+                            o
+                        @else
+                            @if(count($thursday) == 1)
+                                @if(!$thursday[0]->closed)
+                                    @if($thursday->opentime_in_min >= $i && $i <= $thursday->closetime_in_min)
+                                        <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'thursday', 'time' => $i]) }}">Boek een afspraak</a>
+                                    @else
+                                        o
+                                    @endif
+                                @else
+                                    o
+                                @endif
+                            @else
+                                @if(($thursday[0]->opentime_in_min <= $i && $i < $thursday[0]->closetime_in_min) || ($thursday[1]->opentime_in_min <= $i && $i < $thursday[1]->closetime_in_min))
+                                    <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'thursday', 'time' => $i]) }}">Boek een afspraak</a>
+                                @else
+                                    o
+                                @endif
+                            @endif
+                        @endif
+                    </td>
+
+                    <td>
+                        @if($startofweek->addDays(1)->isPast())
+                            @php $beforetoday = false @endphp
+                            o
+                        @else
+                            @if(count($friday) == 1)
+                                @if(!$friday[0]->closed)
+                                    @if($friday->opentime_in_min >= $i && $i <= $friday->closetime_in_min)
+                                        <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'friday', 'time' => $i]) }}">Boek een afspraak</a>
+                                    @else
+                                        o
+                                    @endif
+                                @else
+                                    o
+                                @endif
+                            @else
+                                @if(($friday[0]->opentime_in_min <= $i && $i < $friday[0]->closetime_in_min) || ($friday[1]->opentime_in_min <= $i && $i < $friday[1]->closetime_in_min))
+                                    <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'friday', 'time' => $i]) }}">Boek een afspraak</a>
+                                @else
+                                    o
+                                @endif
+                            @endif
+                        @endif
+                    </td>
+
+                    <td>
+                        @if($startofweek->addDays(1)->isPast())
+                            @php $beforetoday = false @endphp
+                            o
+                        @else
+                            @if(count($saturday) == 1)
+                                @if(!$saturday[0]->closed)
+                                    @if($saturday->opentime_in_min >= $i && $i <= $saturday->closetime_in_min)
+                                        <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'saturday', 'time' => $i]) }}">Boek een afspraak</a>
+                                    @else
+                                        o
+                                    @endif
+                                @else
+                                    o
+                                @endif
+                            @else
+                                @if(($saturday[0]->opentime_in_min <= $i && $i < $saturday[0]->closetime_in_min) || ($saturday[1]->opentime_in_min <= $i && $i < $saturday[1]->closetime_in_min))
+                                    <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'saturday', 'time' => $i]) }}">Boek een afspraak</a>
+                                @else
+                                    o
+                                @endif
+                            @endif
+                        @endif
+                    </td>
+
+                    <td>
+                        @if($startofweek->addDays(1)->isPast())
+                            @php $beforetoday = false @endphp
+                            o
+                        @else
+                            @if(count($sunday) == 1)
+                                @if(!$sunday[0]->closed)
+                                    @if($sunday->opentime_in_min >= $i && $i <= $sunday->closetime_in_min)
+                                        <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'sunday', 'time' => $i]) }}">Boek een afspraak</a>
+                                    @else
+                                        o
+                                    @endif
+                                @else
+                                    o
+                                @endif
+                            @else
+                                @if(($sunday[0]->opentime_in_min <= $i && $i < $sunday[0]->closetime_in_min) || ($sunday[1]->opentime_in_min <= $i && $i < $sunday[1]->closetime_in_min))
+                                    <a href="{{ route('appointmentform', ['name' => $name, 'id' => $id, 'day' => 'sunday', 'time' => $i]) }}">Boek een afspraak</a>
+                                @else
+                                    o
+                                @endif
+                            @endif
+                        @endif
+                    </td>
+
+
+                </tr>
+                
             @endfor
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
 
-        @endforeach
+    </table>
 
-
-
-        <h3>Dinsdag</h3>
-        @foreach($tuesdayhours as $hour)
-
-            @if($hour->closed)
-                <p>Gesloten</p>
-            @else
-                <p>{{ $hour->opentime }} - {{ $hour->closetime }}</p>
-            @endif
-
-        @endforeach
-
-
-
-        <h3>Woensdag</h3>
-        @foreach($wednesdayhours as $hour)
-
-            @if($hour->closed)
-                <p>Gesloten</p>
-            @else
-                <p>{{ $hour->opentime }} - {{ $hour->closetime }}</p>
-            @endif
-
-        @endforeach
-
-
-
-        <h3>Donderdag</h3>
-        @foreach($thursdayhours as $hour)
-
-            @if($hour->closed)
-                <p>Gesloten</p>
-            @else
-                <p>{{ $hour->opentime }} - {{ $hour->closetime }}</p>
-            @endif
-
-        @endforeach
-
-
-
-        <h3>Vrijdag</h3>
-        @foreach($fridayhours as $hour)
-
-            @if($hour->closed)
-                <p>Gesloten</p>
-            @else
-                <p>{{ $hour->opentime }} - {{ $hour->closetime }}</p>
-            @endif
-
-        @endforeach
-
-
-
-        <h3>Zaterdag</h3>
-        @foreach($saturdayhours as $hour)
-
-            @if($hour->closed)
-                <p>Gesloten</p>
-            @else
-                <p>{{ $hour->opentime }} - {{ $hour->closetime }}</p>
-            @endif
-
-        @endforeach
-
-
-
-        <h3>Zondag</h3>
-        @foreach($sundayhours as $hour)
-
-            @if($hour->closed)
-                <p>Gesloten</p>
-            @else
-                <p>{{ $hour->opentime }} - {{ $hour->closetime }}</p>
-            @endif
-
-        @endforeach
 
 
 @stop
