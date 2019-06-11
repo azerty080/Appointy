63ffcf9be7f03eea1d4284914ff40d5e68291194
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-06-12 01:26:18 +0200

    added bookmarks on homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,39 +8,63 @@
 
 
     @if(session()->has('logged_in'))
-        <h2>Ingelogd als {{ session()->get('user_type') }} {{ session()->get('user_name') }}</h2>
+        <h2>Ingelogd als @if(session()->get('user_type') == 'klant') {{ session()->get('user_data')->firstname }} {{ session()->get('user_data')->lastname }} @else {{ session()->get('user_data')->name }} @endif</h2>
+
+        @if(session()->get('user_type') == 'klant')
+
+            @if(count($bookmarks) > 0)
+                <h3>Favorieten</h3>
+            @endif
+
+            @foreach($bookmarks as $bookmark)
+
+                <div>
+                    <a href="{{ route('businessdetail', ['name' => $bookmark->business->name, 'id' => $bookmark->business_id]) }}">
+                        <p>Naam: {{ $bookmark->business->name }}</p>
+                        <p>Beroep: {{ $bookmark->business->profession }}</p>
+                        
+                        <p>Gemeente: {{ $bookmark->business->user->township }}</p>
+                        <p>Adres: {{ $bookmark->business->user->address }}</p>
+                    </a>
+                </div>
+
+            @endforeach
+
+        @endif
+    
     @else 
         <h2>Ingelogd als gast</h2>
     @endif
 
 
 
-    
-    <form method="GET" action="{{ route('searchresults') }}">
+    @if(session()->get('user_type') == 'klant' || !session()->has('logged_in'))
+        <form method="GET" action="{{ route('searchresults') }}">
 
-        <h2>Zoek zaak</h2>
-        
-        <div class="inputGroup">
-            <div class="soloInput">
-                <label>Naam van de zaak</label>
-                <input name="name" type="text">
+            <h2>Zoek zaak</h2>
+            
+            <div class="inputGroup">
+                <div class="soloInput">
+                    <label>Naam van de zaak</label>
+                    <input name="name" type="text">
+                </div>
+
+                <p>OF</p>
+
+                <div class="soloInput">
+                    <label>Beroep</label>
+                    <input name="profession" type="text">
+                </div>
+
+                <div class="soloInput">
+                    <label>Gemeente</label>
+                    <input name="township" type="text">
+                </div>
             </div>
-
-            <p>OF</p>
-
-            <div class="soloInput">
-                <label>Beroep</label>
-                <input name="profession" type="text">
-            </div>
-
-            <div class="soloInput">
-                <label>Gemeente</label>
-                <input name="township" type="text">
-            </div>
-        </div>
-        
-        <button type="submit">Zoek</button>
-    </form>
+            
+            <button type="submit">Zoek</button>
+        </form>
+    @endif
 
 
 @stop
