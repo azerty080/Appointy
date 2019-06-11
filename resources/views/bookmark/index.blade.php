a2814a07d57fcd3b7112d251e403c422a00f253f
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-06-12 01:22:15 +0200

    added bookmark view

---
+++ resources/views/bookmark/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title', 'Favorieten')
+
+
+@section('content')
+
+    <h1>Favorieten</h1>
+    
+
+    @foreach($bookmarks as $bookmark)
+
+        <div>
+            <a href="{{ route('businessdetail', ['name' => $bookmark->business->name, 'id' => $bookmark->business_id]) }}">
+                <p>Naam: {{ $bookmark->business->name }}</p>
+                <p>Beroep: {{ $bookmark->business->profession }}</p>
+                
+                <p>Gemeente: {{ $bookmark->business->user->township }}</p>
+                <p>Adres: {{ $bookmark->business->user->address }}</p>
+            </a>
+        </div>
+
+    @endforeach
+
+
+    
+
+@stop
