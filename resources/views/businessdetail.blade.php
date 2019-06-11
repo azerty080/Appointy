b77cfeef75a8cb7f830a1fdf285b86ee8145fa1d
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-06-12 01:26:01 +0200

    added bookmark button and allow guest functionality

--- resources/views/businessdetail.blade.php
+++ resources/views/businessdetail.blade.php
@@ -20,6 +20,38 @@
     <p>Email: {{ $business->user->email }}</p>
 
 
-    <a href="{{ route('businesscalendar', ['name' => $business->name, 'id' => $business->id, 'addedweek' => 0]) }}">Maak een afspraak</a>
+
+    @if(session()->has('logged_in'))
+        @if($isbookmarked)
+            <form method="POST" action="{{ route('removebookmark', ['name' => $business->name, 'id' => $business->id]) }}">
+                @csrf
+
+                <input name="business_id" type="number" value="{{ $business->id }}" hidden>
+                
+                <button type="submit">Verwijder van favorieten</button>
+            </form>
+        @else
+            <form method="POST" action="{{ route('addbookmark', ['name' => $business->name, 'id' => $business->id]) }}">
+                @csrf
+
+                <input name="business_id" type="number" value="{{ $business->id }}" hidden>
+                
+                <button type="submit">Toevoegen aan favorieten</button>
+            </form>
+        @endif
+    @else
+        <p>Je moet ingelogd zijn om favorieten toe te voegen</p>
+    @endif
+
+    @if($business->allow_guests)
+        <a href="{{ route('businesscalendar', ['name' => $business->name, 'id' => $business->id, 'addedweek' => 0]) }}">Maak een afspraak</a>
+    @else
+        @if(session()->has('logged_in'))
+            <a href="{{ route('businesscalendar', ['name' => $business->name, 'id' => $business->id, 'addedweek' => 0]) }}">Maak een afspraak</a>
+        @else
+            <p>Je moet ingelogd zijn om bij deze zaak een afspraak te maken</p>
+        @endif
+    @endif
+
 
 @stop
