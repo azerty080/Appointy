592debbd117a8fc6e3725abad473690775f3eb6f
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-06-12 01:27:00 +0200

    removed email and phonenumber from searchresults

--- resources/views/searchresults.blade.php
+++ resources/views/searchresults.blade.php
@@ -13,14 +13,8 @@
                 <p>Naam: {{ $business->name }}</p>
                 <p>Beroep: {{ $business->profession }}</p>
                 
-                <p>Beschrijving: {{ $business->description }}</p>
-
                 <p>Gemeente: {{ $business->user->township }}</p>
                 <p>Adres: {{ $business->user->address }}</p>
-
-                <p>Telefoonnummer: {{ $business->user->phonenumber }}</p>
-
-                <p>Email: {{ $business->user->email }}</p>
             </a>
         </div>
 
