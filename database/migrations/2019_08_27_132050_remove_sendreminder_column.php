7a5c2bd8a7ba9cfcc7c91481bf71d386863557ca
Niels van Nimmen <niels.van.nimmen@gmail.com>
2019-08-29 01:45:03 +0200

    added remove column migration

---
+++ database/migrations/2019_08_27_132050_remove_sendreminder_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveSendreminderColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->dropColumn('sendreminder');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->boolean('sendreminder');
+        });
+    }
+}
